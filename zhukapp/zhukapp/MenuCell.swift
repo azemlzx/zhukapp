5c3c89b63951700951e1433b66febf966d82d88c
azemlzx <azemlzx@gmail.com>
2017-10-04 23:44:02 +0300

    add menu list

---
+++ zhukapp/zhukapp/MenuCell.swift
@@ -0,0 +1,15 @@
+//
+//  MenuCell.swift
+//  zhukapp
+//
+//  Created by Alexander Zemlyansky on 04.10.17.
+//  Copyright © 2017 Alexander Zemlyansky. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    @IBOutlet weak var tabnameMenuCell: UILabel!
+    
+}
--- zhukapp/zhukapp/ViewControllerTable.swift
+++ zhukapp/zhukapp/ViewControllerTable.swift
@@ -19,8 +19,16 @@ class ViewControllerTable: UIViewController,UITableViewDelegate, UITableViewData
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableViewMenu.dataSource = self
+        self.tableViewMenu.delegate = self
+        
+        self.tableViewFirst.dataSource = self
+        self.tableViewFirst.delegate = self
+        
         menuView.layer.shadowOpacity = 1
         menuView.layer.shadowRadius = 6
+        tableViewMenu.tableFooterView = UIView(frame: CGRect.zero)
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,13 +37,38 @@ class ViewControllerTable: UIViewController,UITableViewDelegate, UITableViewData
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        
+        var countS : Int = 0
+        
+        if (tableView == tableViewFirst){
+            countS = 10
+        }else if (tableView == tableViewMenu){
+            countS = 3
+        }
+        return countS
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //guard (tableView.dequeueReusableCell(withIdentifier: "Cell") as? TableViewCell) != nil else {
-            return UITableViewCell()
-        //}
+        
+        if (tableView == tableViewFirst){
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? TableViewCell else {
+                return UITableViewCell()
+            }
+        }else if (tableView == tableViewMenu){
+            guard let cellMenu = tableView.dequeueReusableCell(withIdentifier: "CellMenu") as? MenuCell else {
+                return UITableViewCell()
+            }
+            switch (indexPath.row){
+            case 0 : cellMenu.tabnameMenuCell.text = "Продажи"
+            case 1 : cellMenu.tabnameMenuCell.text = "Заявки"
+            case 2 : cellMenu.tabnameMenuCell.text = "Согласования продаж"
+            default:
+               cellMenu.tabnameMenuCell.text =  ""
+            }
+            return cellMenu
+            
+        }
+        return UITableViewCell()
     }
     
     
