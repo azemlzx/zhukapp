2759a77826b4c9124e6d6287e370b0dea6c3c338
azemlzx <azemlzx@gmail.com>
2018-02-04 01:45:48 +0200

    add authorization screen

--- zhukapp/zhukapp/ViewController.swift
+++ zhukapp/zhukapp/ViewController.swift
@@ -10,8 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var singinButton: UIButton!
+    @IBOutlet weak var loginText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // sing in button
+        singinButton.backgroundColor = .clear
+        singinButton.layer.cornerRadius = 15
+        singinButton.layer.borderWidth = 3
+        singinButton.layer.borderColor = UIColor.white.cgColor
+        // login textfild
+        let imageViewAvatar = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        let avatar = UIImage(named: "avatar.png")
+        imageViewAvatar.image = avatar
+        imageViewAvatar.alpha = 0.4
+        loginText.leftView = imageViewAvatar
+        loginText.leftViewMode = UITextFieldViewMode.always
+        loginText.leftViewMode = .always
+        loginText.layer.cornerRadius = 12
+        loginText.layer.masksToBounds = true
+        loginText.placeholder = "Логін"
+        // password textfild
+        let imageViewKey = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        let key = UIImage(named: "key.png")
+        imageViewKey.image = key
+        imageViewKey.alpha = 0.4
+        passwordText.leftView = imageViewKey
+        passwordText.leftViewMode = UITextFieldViewMode.always
+        passwordText.leftViewMode = .always
+        passwordText.layer.cornerRadius = 12
+        passwordText.layer.masksToBounds = true
+        passwordText.placeholder = "Пароль"
     }
     
     override func didReceiveMemoryWarning() {
