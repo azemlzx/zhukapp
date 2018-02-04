ec3b7649896a4b43775a116933046de9dc2afd0f
azemlzx <azemlzx@gmail.com>
2018-02-04 15:29:49 +0200

    add Designable and DataManager

---
+++ zhukapp/zhukapp/Manager/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  zhukapp
+//
+//  Created by Alexander Zemlyansky on 04.02.18.
+//  Copyright © 2018 Alexander Zemlyansky. All rights reserved.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+
+}
